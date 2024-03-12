eb75d9f4046e303ce64e943165fe0e21890198a9
tekinnigar1 <148658555+tekinnigar1@users.noreply.github.com>
2024-03-12 21:03:50 +0300

    Updates

---
+++ precache-manifest.1edcf9ec09fd79d7fa94fdece680847f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b53d64e1aecd2f77b94b25b91e9efa",
+    "url": "/gcnew/index.html"
+  },
+  {
+    "revision": "6342529dfa15515d520b",
+    "url": "/gcnew/static/js/2.b7cdb8dc.chunk.js"
+  },
+  {
+    "revision": "1c56b18a0dbaa5f73ba9",
+    "url": "/gcnew/static/js/main.bab30bab.chunk.js"
+  },
+  {
+    "revision": "25390558de5c469050e7",
+    "url": "/gcnew/static/js/runtime-main.414fd0e9.js"
+  }
+]);
